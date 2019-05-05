a86237e4122726986b1086f7843c2d9e732eca8b
IuSpet <1037821259@qq.com>
2019-05-05 18:41:17 +0800

    语法分析器类

---
+++ Parser.h
@@ -0,0 +1,25 @@
+/***
+
+***/
+#ifndef Parser_h
+#define Parser_h
+#include"utlib.h"
+class Parser
+{
+public:
+	Parser();
+	~Parser();
+
+private:
+
+};
+
+Parser::Parser()
+{
+}
+
+Parser::~Parser()
+{
+}
+#endif // !Parser_h
+#pragma once
