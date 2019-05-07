d5a90507fb0493c6b820b2c61733d97515d4381b
IuSpet <1037821259@qq.com>
2019-05-07 21:36:20 +0800

    去除左递归ing

--- Parser.cpp
+++ Parser.cpp
@@ -21,9 +21,15 @@ void Parser::print_grammar0()
 	get_grammar();
 	for (auto l : grammar0)
 	{
+		bool first = true;
 		for (auto t : l)
 		{
-			std::cout << t << "  ";
+			if (first)
+			{
+				std::cout << t << " -> ";
+				first = false;
+			}
+			else std::cout << t << " | ";
 		}
 		std::cout << std::endl << std::endl;
 	}
@@ -37,26 +43,29 @@ void Parser::get_grammar()
 	{
 		std::list<std::string> tmp;
 		int len = strlen(input_buffer);
-		char str[256] = {};
+		std::string str;
 		int pos = 0;
 		for (int i = 0; i < len; i++)
 		{
-			if (input_buffer[i] == ' ') continue;
+			if (input_buffer[i] == ' ' && str.length() == 0) continue;
 			else if (input_buffer[i] == '|')
 			{
+				while (str.back() == ' ') str.pop_back();
 				tmp.push_back(str);
-				memset(str, 0, sizeof(str));
+				str.clear();
 				pos = 0;
 			}
 			else if (input_buffer[i] == -95)
 			{
+				while (str.back() == ' ') str.pop_back();
 				tmp.push_back(str);
-				memset(str, 0, sizeof(str));
+				str.clear();
 				pos = 0;
 				i++;
 			}
-			else str[pos++] = input_buffer[i];
+			else str.push_back(input_buffer[i]);
 		}
+		while (str.back() == ' ' || str.back() == '\n') str.pop_back();
 		tmp.push_back(str);
 		grammar0.push_back(tmp);
 	}
@@ -65,6 +74,41 @@ void Parser::get_grammar()
 void Parser::Eliminate_left_recursion()
 {
 	//TO DO:消除grammar0里的左递归
+	for (auto p = grammar0.begin(); p != grammar0.end(); p++)
+	{
+		for (auto j = grammar0.begin(); j != p ; j++)
+		{
+			//替换A产生式中的Vn
+			std::list<std::string> &A = *p;			//文法A
+			std::list<std::string> &B = *j;			//文法B
+			std::string &Vn = B.front();			//文法B的开头
+			auto pA = A.begin(); pA++;
+			//auto pB = B.begin(); pB++;
+			for (; pA != A.end(); pA++)				
+			{
+				std::string &production = *pA;		//文法A中的产生式
+				std::string tmp;					//提取production中的非终结符
+				int subbg = 0, subend = 0;			//记录tmp出现的位置保留前缀后缀
+				for (char c : production)
+				{
+					if (c != ' ') tmp.push_back(c);
+					else							//提取出了一个完整的非终结符
+					{
+						if (Vn == tmp)				//该非终结符是前面的开始符号
+						{
+							std::string prefix = production.substr();
+							std::string suffix = production.substr();
+							auto pB = B.begin(); pB++;
+							for (; pB != B.end(); pB++)		//将production替换为B中的产生式
+							{
+								  
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 std::string Parser::get_next_token()
